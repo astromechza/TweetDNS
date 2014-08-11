1c010d5590af7165b0e147b7e67aacc46bdf3360
Ben Meier <benmeier42@gmail.com>
2014-08-11 23:22:27 +0200

    filled out loading and saving of config and testing

--- tests/configuration_test.py
+++ tests/configuration_test.py
@@ -1,15 +1,21 @@
 import unittest
+import os
+import tempfile
 
 from tweet_dns import configuration
 
 class ConfigurationTest(unittest.TestCase):
+
+    def build_fake_config(self, **kwargs):
+        kwargs['custom_path'] = os.path.join(tempfile.mkdtemp(), '.config', 'clacks', 'settings')
+        return configuration.Config(**kwargs)
 
     def test_can_touch(self):
         self.assert_(configuration.config.__class__.__name__ == 'Config')
 
     def test_access(self):
 
-        test_config = configuration.Config()
+        test_config = self.build_fake_config(auto_load=True)
 
         test_config.set('key1', 'key2', 'key3', 'value')
         test_config.set('key1', 'key2', 'key4', 'value2')
@@ -23,7 +29,7 @@ class ConfigurationTest(unittest.TestCase):
     def test_example(self):
         # set up
 
-        test_config = configuration.Config()
+        test_config = self.build_fake_config(auto_load=True)
 
         test_config.set('users', 1, 'name', 'Joe Soap')
         test_config.set('users', 1, 'email', '1.example.com')
@@ -46,3 +52,53 @@ class ConfigurationTest(unittest.TestCase):
             self.assert_(test_config.get('users', i+1, 'name') != None)
             self.assert_(test_config.get('users', i+1, 'city') != None)
             self.assert_(test_config.get('users', i+1, 'email') == "%s.example.com" % (i+1))
+
+    def test_blank_new_config(self):
+        c = self.build_fake_config()
+
+        # file should not exist
+        self.assert_(not os.path.exists(c.file_path))
+
+        # load
+        c.load()
+
+        # config should be blank
+        self.assert_(c._tree == {})
+
+        # should not be able to load anything
+        with self.assertRaises(KeyError):
+            c.get('a')
+
+        # save
+        c.save()
+
+        # file should exist
+        self.assert_(os.path.exists(c.file_path))
+
+    def test_save_before_load(self):
+        c = self.build_fake_config()
+        with self.assertRaises(RuntimeError):
+            c.save()
+
+    def test_double_load(self):
+        c = self.build_fake_config(auto_load=True)
+        with self.assertRaises(RuntimeError):
+            c.load()
+
+    def test_get_and_set(self):
+        # create fake config
+        c = self.build_fake_config(auto_load=True)
+
+        # test that set and get work
+        c.set('a', 'b', 'hello')
+        self.assert_(c.get('a', 'b') == 'hello')
+        self.assert_(c._tree == {'a':{'b':'hello'}})
+
+        # save
+        c.save()
+
+        # open new config with same file
+        c2 = configuration.Config(custom_path=c.file_path, auto_load=True)
+
+        # check that contents are the same
+        self.assert_(c.get('a', 'b') == 'hello')
--- tweet_dns/configuration.py
+++ tweet_dns/configuration.py
@@ -1,18 +1,51 @@
+import os
+import json
+
 from .exceptions import ConfigKeyError
 
 class Config(object):
-    """
 
-    """
+    _DEFAULT_CONFIG_PATH = '~/.config/tweet_dns/settings'
 
-    def __init__(self):
-        self._tree = {}
+    def __init__(self, custom_path=_DEFAULT_CONFIG_PATH, auto_load=False):
+        self.loaded = False
+        self.file_path = os.path.expanduser(custom_path)
+        if auto_load:
+            self.load()
+
+    def load(self):
+        if self.loaded:
+            raise RuntimeError ("Config has already been loaded!")
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as text_file:
+                self._tree = json.loads(text_file.read())
+        else:
+            self._tree = {}
+        self.loaded = True
+
+    def save(self):
+        if not self.loaded:
+            raise RuntimeError("Config has not been loaded yet!")
+
+        content = json.dumps(self._tree)
+
+        # make sure correct directories exist
+        directory = os.path.dirname(self.file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # save to file
+        with open(self.file_path, "w") as text_file:
+            text_file.write(content)
 
     def set(self, param_one, param_two, *other_params):
         """
         Set the given setting value. The path is given as a list of nodes in a
         tree. So set('a', 'b', 'value') will result in {'a':{'b': 'value'}}
         """
+
+        if not self.loaded:
+            raise RuntimeError("Config has not been loaded yet!")
 
         params = (param_one, param_two) + other_params
         str_params = [str(p) for p in params[:-1]]
@@ -31,6 +64,9 @@ class Config(object):
     def get(self, key_one, *other_keys):
         """
         """
+
+        if not self.loaded:
+            raise RuntimeError("Config has not been loaded yet!")
 
         keys = tuple([key_one]) + other_keys
         str_keys = [str(k) for k in keys]
